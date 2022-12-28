e7e771f20965255f90cf4886fa3dd4003d3c6359
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2022-12-28 15:04:21 -0500

    Refactor project

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,12 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
-  cache: false,
+  entry: {
+    index: './src/index.js',
+  },
+  devtool: 'inline-source-map',
   devServer: {
-    static: './dist',
-    watchFiles: ['src/*.html'],
-    hot: true,
+    static: './docs',
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -16,15 +16,26 @@ module.exports = {
     }),
   ],
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'docs'),
     clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
       },
     ],
   },
