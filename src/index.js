652d1c0062313f102bf526bde08c4a135dd23f4b
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2022-12-30 14:12:13 -0500

    Testing CellView class.

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@ import './style.css';
 import CellView from './modules/CellView.js';
 
 const container = document.getElementById('cont');
-const cell = new CellView(5);
-container.appendChild(cell.ui);
+
+let cell;
+
+for (let i = 0; i < 9; i += 1) {
+  for (let j = 0; j < 9; j += 1) {
+    cell = new CellView(j);
+    container.appendChild(cell.ui);
+  }
+}
